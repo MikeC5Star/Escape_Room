87409b14bb6a724bf99754432ee137b6415a3568
mike_d_star <mrowe0@hotmail.com>
2019-02-15 13:53:09 +0000

    L76 EscapeRoom ... Trigger volume set to playercontroller pawn.

--- Escape_Room/Source/Escape_Room/PositionReport.cpp
+++ Escape_Room/Source/Escape_Room/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include <GameFramework/Actor.h>
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -18,8 +19,19 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+
+	UE_LOG(LogTemp, Warning, TEXT("Position Reporter"));
+
+	AActor* act = GetOwner();
+	FString name = act->GetName();
+	FVector pos = act->GetActorLocation();
+	FString posStr = pos.ToCompactString();
+	UE_LOG(LogTemp, Warning, TEXT("Position Reporter: Actor name is %s & position is %s"), *name, *posStr);
+
+	FRotator rotation = act->GetActorRotation();
+	FRotator rot = FRotator(0.0f, 90.0f, 0.0f);
+	FRotator both = rotation + rot;
+	act->SetActorRotation(both);
 }
 
 
@@ -28,6 +40,5 @@ void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActor
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
 }
 
